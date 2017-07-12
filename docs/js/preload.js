cfee715aca4c82fa3479f900c08b887b14ded08a
ozeki <hhdfgg@users.noreply.github.com>
2017-07-12 10:42:08 +0900

    Update preload.js

--- docs/js/preload.js
+++ docs/js/preload.js
@@ -6,7 +6,9 @@
  * @copyright 2017 YuTanaka@AmuseOne
  */
 function preload() {
-
+        this.load.baseURL = "https://hhdfgg.github.io/second/";
+        this.load.crossOrigin = "anonymous";
+    
     game.load.image('star', '/assets/images/star.png');
     game.load.spritesheet('dude', '/assets/images/dude.png',32,48);
     game.load.image('baddie', '/assets/images/baddie.png');
