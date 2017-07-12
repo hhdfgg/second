d6164cbac586e84099a2dfb1a26a392313e4fd17
ozeki <hhdfgg@users.noreply.github.com>
2017-07-12 23:34:59 +0900

    Update preload.js

--- docs/js/preload.js
+++ docs/js/preload.js
@@ -9,8 +9,8 @@ function preload() {
     game.load.baseURL = "https://hhdfgg.github.io/second/";
     game.load.crossOrigin = "anonymous";
     
-    game.load.image('star', '/assets/images/star.png');
-    game.load.spritesheet('dude', '/assets/images/dude.png',32,48);
-    game.load.image('baddie', '/assets/images/baddie.png');
+    game.load.image('star', 'assets/images/star.png');
+    game.load.spritesheet('dude', 'assets/images/dude.png',32,48);
+    game.load.image('baddie', 'assets/images/baddie.png');
 
 }
